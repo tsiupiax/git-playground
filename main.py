78ccd493eee902ad7ebf31734f8eee0dd81ca444
Solomiia <stsiupiaks@gmail.com>
2022-12-02 16:33:34 +0200

    add

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ word = words[random.randrange(0, len(words))]
 
 print(f"Can you make up {WORDS_TO_WIN} words from letters in word provided by me?")
 print(f"Your word is '{word}'")
+print()
 
 
 while not is_game_over():
